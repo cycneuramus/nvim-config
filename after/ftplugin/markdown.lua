912cad25f9045449e3a67a0202bb0a55bbcb9444
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2025-04-10 12:56:02 +0200

    Add no-neck-pain.nvim

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -11,5 +11,4 @@ map_surround("<C-l>", "[", "]()", "Link")
 -- Continue list items on newline (using commentstrings)
 vim.opt_local.comments = { "b:*", "b:-", "b:+", "n:>" }
 vim.opt_local.formatoptions:append("ro")
-
--- vim.api.nvim_command("ZenMode")
+vim.opt_local.number = false
