16087e9fde905bdf6f8d1c4efff1e76aa1308ca2
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2025-04-10 12:24:41 +0200

    Restructure mason setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,28 +1,34 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		config = function()
+			require("mason").setup()
+		end,
 	},
-	event = "BufReadPre",
-	config = function()
-		require("mason").setup()
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"ansible-language-server",
-				-- "ansible-lint",
-				"bash-language-server",
-				"basedpyright",
-				"dockerfile-language-server", -- TODO:
-				"docker-compose-language-service", -- TODO:
-				"goimports",
-				"gopls",
-				"hadolint",
-				"lua-language-server",
-				"nimlangserver",
-				"shellcheck",
-				"shfmt",
-				"stylua",
-			},
-		})
-	end,
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		cmd = "MasonToolsUpdate",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"ansible-language-server",
+					"bash-language-server",
+					"basedpyright",
+					"dockerfile-language-server", -- TODO: setup
+					"docker-compose-language-service", -- TODO: setup
+					"goimports", -- FIXME make conditional on go version
+					"gopls", -- FIXME make conditional on go version
+					"hadolint", -- TODO: setup (or perhaps skip altogether)
+					"lua-language-server",
+					"nimlangserver",
+					"shellcheck",
+					"shfmt",
+					"stylua",
+				},
+				auto_update = true,
+			})
+		end,
+	},
 }
