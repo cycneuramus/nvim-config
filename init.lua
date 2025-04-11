97a29eda46bd1d6ff925c435aa93975653c78298
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2025-04-11 17:07:41 +0200

    Startup check reordering

--- init.lua
+++ init.lua
@@ -1,13 +1,12 @@
+require("config.options")
+
 if vim.fn.has("nvim-0.11") ~= 1 then
-    vim.o.cmdheight = 0
-    vim.o.loadplugins = false
     vim.o.loadplugins = false
     vim.notify("This config requires neovim 0.11 and above", vim.log.levels.WARN)
     return
 end
 
 require("config.lazy")
-require("config.options")
 require("config.keymaps")
 require("config.autocmd")
 require("config.commands")
