b6fcbe517a30f42bbb91d195e2701c647304d6c7
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2025-05-07 09:43:16 +0200

    Tweak nimlangserver

--- lsp/nimlangserver.lua
+++ lsp/nimlangserver.lua
@@ -1,17 +1,21 @@
 local lsputil = require("utils/lsp")
 
 vim.lsp.config.nimlangserver = {
-    cmd = { "nimlangserver" },
-    cmd_env = {
-        HOME = vim.fn.expand("~/.local/share/nim"),
-    },
-    filetypes = { "nim" },
-    root_dir = lsputil.root_globs({ "*.nimble" }),
-    settings = {
-        nim = {
-            notificationVerbosity = "error",
-            autoCheckFile = true,
-            autoCheckProject = false,
-        },
-    },
+	cmd = { "nimlangserver" },
+	cmd_env = {
+		HOME = vim.fn.expand("~/.local/share/nim"),
+	},
+	filetypes = { "nim" },
+	root_dir = lsputil.root_globs({ "*.nimble" }),
+	single_file_support = true,
+	settings = {
+		nim = {
+			notificationVerbosity = "none",
+			formatOnSave = false,
+			checkOnSave = true,
+			autoCheckFile = true,
+			autoCheckProject = false,
+			autoRestart = true,
+		},
+	},
 }
