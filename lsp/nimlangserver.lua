6110a3b8bad6517c5e5e0ce36a72d3b7d5e28d95
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2025-10-21 11:48:21 +0200

    Accommodate choosenim -> grabnim switch

--- lsp/nimlangserver.lua
+++ lsp/nimlangserver.lua
@@ -1,22 +1,22 @@
 local lsputil = require("utils/lsp")
 
 vim.lsp.config.nimlangserver = {
-	cmd = { "nimlangserver" },
-	cmd_env = {
-		HOME = vim.fn.expand("~/.local/share/nim"),
-	},
-	filetypes = { "nim" },
-	root_dir = lsputil.root_globs({ "*.nimble" }),
-	single_file_support = true,
-	settings = {
-		nim = {
-			nimsuggestPath = vim.fn.expand("~/.local/share/nim/.nimble/bin/nimsuggest"),
-			notificationVerbosity = "none",
-			formatOnSave = false,
-			checkOnSave = true,
-			autoCheckFile = true,
-			autoCheckProject = false,
-			autoRestart = true,
-		},
-	},
+    cmd = { "nimlangserver" },
+    -- cmd_env = {
+    -- 	HOME = vim.fn.expand("~/.local/share/nim"),
+    -- },
+    filetypes = { "nim" },
+    root_dir = lsputil.root_globs({ "*.nimble" }),
+    single_file_support = true,
+    settings = {
+        nim = {
+            nimsuggestPath = vim.fn.expand("~/.local/share/nim/.nimble/bin/nimsuggest"),
+            notificationVerbosity = "none",
+            formatOnSave = false,
+            checkOnSave = true,
+            autoCheckFile = true,
+            autoCheckProject = false,
+            autoRestart = true,
+        },
+    },
 }
