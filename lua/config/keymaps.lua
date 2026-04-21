3b67eed6a5ddc65882e08e83339d55da45800db2
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2026-04-21 13:29:22 +0200

    Remove unused keymaps for moving lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,12 +34,6 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
--- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
-
 -- Escaping
 local nohEsc = "<cmd>nohlsearch<CR><esc>"
 map("n", "<ESC>", nohEsc, { desc = "Clear search highlight" })
@@ -62,43 +56,43 @@ map("n", "<leader>:", "<cmd>FzfLua command_history<CR>", { desc = "Command histo
 
 -- Map j and k to gj/gk, but only when no count is given
 map("n", "j", function()
-	local count = vim.v.count
-	if count == 0 then
-		return "gj"
-	elseif count > 5 then
-		return "m'" .. count .. "j"
-	else
-		return count .. "j"
-	end
+    local count = vim.v.count
+    if count == 0 then
+        return "gj"
+    elseif count > 5 then
+        return "m'" .. count .. "j"
+    else
+        return count .. "j"
+    end
 end, { expr = true, desc = "Visual line movement (j)" })
 
 map("n", "k", function()
-	local count = vim.v.count
-	if count == 0 then
-		return "gk"
-	elseif count > 5 then
-		return "m'" .. count .. "k"
-	else
-		return count .. "k"
-	end
+    local count = vim.v.count
+    if count == 0 then
+        return "gk"
+    elseif count > 5 then
+        return "m'" .. count .. "k"
+    else
+        return count .. "k"
+    end
 end, { expr = true, desc = "Visual line movement (k)" })
 
 -- Toggle virtual lines diagnostics on-demand
 map("n", "<leader>d", function()
-	vim.diagnostic.config({
-		virtual_lines = not vim.diagnostic.config().virtual_lines,
-	})
+    vim.diagnostic.config({
+        virtual_lines = not vim.diagnostic.config().virtual_lines,
+    })
 end)
 
 -- LSP
 vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP actions",
-	callback = function()
-		map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-		map("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
-		map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
-		map("n", "gs", vim.lsp.buf.signature_help, { desc = "Signature help" })
-		map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
-		map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-	end,
+    desc = "LSP actions",
+    callback = function()
+        map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+        map("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
+        map("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+        map("n", "gs", vim.lsp.buf.signature_help, { desc = "Signature help" })
+        map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+        map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+    end,
 })
